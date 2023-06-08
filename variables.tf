d7c3e978c036629cd076b468ac149f32413ce663
smithjustinm <smith.justinmatthew@gmail.com>
2023-06-07 19:26:43 -0500

    add hcl file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+   type = string
+   description = "Azure Resource Group Name"
+   default = "justin-interview-wus2-rg"
+ }
+
+ variable "azure_location" {
+   type = string
+   description = "Azure Location"
+   default = "West US 2"
+ }
