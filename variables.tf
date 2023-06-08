3973caeb077fdfda50d075f186f901e0d89d930c
smithjustinm <smith.justinmatthew@gmail.com>
2023-06-07 19:34:39 -0500

    specif tf version

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-   type = string
-   description = "Azure Resource Group Name"
-   default = "justin-interview-wus2-rg"
- }
+  type        = string
+  description = "Azure Resource Group Name"
+  default     = "justin-interview-wus2-rg"
+}
 
- variable "azure_location" {
-   type = string
-   description = "Azure Location"
-   default = "West US 2"
- }
+variable "azure_location" {
+  type        = string
+  description = "Azure Location"
+  default     = "West US 2"
+}
--- versions.tf
+++ versions.tf
@@ -1,13 +1,14 @@
 # Configure Terraform
 terraform {
-   required_providers {
-     azurerm = {
-       source  = "hashicorp/azurerm"
-       version = "=3.59.0"
-     }
-     azuread = {
-       source  = "hashicorp/azuread"
-       version = "~> 2.39.0"
-     }
-   }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.59.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.39.0"
+    }
+  }
+  required_version = "= 1.4.6"
 }
